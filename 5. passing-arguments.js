9172acd3820396d892f265c1b9b0ebf524ca9519
davidasync <davidasync@gmail.com>
2018-04-27 11:00:17 +0700

    passing argument example

--- 5. passing-arguments.js
+++ 5. passing-arguments.js
@@ -28,3 +28,21 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+// var dice = 3;
+// var sides = 6;
+// var xhr = new XMLHttpRequest();
+// xhr.responseType = 'json';
+// xhr.open("POST", "/graphql");
+// xhr.setRequestHeader("Content-Type", "application/json");
+// xhr.setRequestHeader("Accept", "application/json");
+// xhr.onload = function () {
+//   console.log('data returned:', xhr.response);
+// }
+// var query = `query RollDice($dice: Int!, $sides: Int) {
+//   rollDice(numDice: $dice, numSides: $sides)
+// }`;
+// xhr.send(JSON.stringify({
+//   query: query,
+//   variables: { dice: dice, sides: sides },
+// }));
